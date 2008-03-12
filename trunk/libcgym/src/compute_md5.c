59f802c3cfb0bbade6c0d9233d6649012bf14d25
ioanaverebi <ioanaverebi@1d676674-cb47-0410-9e64-db1d7fe6b3aa>
2008-03-12 23:55:33 +0000

    Compute MD5 for a file

---
+++ trunk/libcgym/src/compute_md5.c
@@ -0,0 +1,42 @@
+#include "libcgym.h"
+#include <openssl/md5.h>
+#include <string.h>
+#include <stdio.h>
+
+char* compute_md5(char* filename){
+	FILE *file;
+	MD5_CTX context;
+	int len;
+	unsigned char buffer[1024], digest[16];
+	char* sir;
+
+	if ((file = fopen (filename, "rb")) == NULL)
+		perror("fopen");
+	else {
+		MD5_Init (&context);
+		while (len = fread (buffer, 1, 1024, file))
+			MD5_Update (&context, buffer, len);
+		MD5_Final (digest, &context);
+
+		fclose (file);
+
+		//printf ("MD5 (%s) = ", filename);
+		//MD5Print (digest);
+		printf ("\n");
+	}
+	int i;
+	sir = malloc(sizeof(digest)+1);
+	for (i = 0; i < 16; i++) { 
+		sprintf(sir + i * 2, "%02x", digest[i]);
+	}
+	return sir;
+	
+}
+
+void MD5Print (unsigned char digest[16]){
+
+  unsigned int i;
+
+  for (i = 0; i < 16; i++)
+	  printf ("%02x", digest[i]);
+}

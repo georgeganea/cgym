339021520b068beb95a2ee3af776a47903a1160a
ioanaverebi <ioanaverebi@1d676674-cb47-0410-9e64-db1d7fe6b3aa>
2008-03-13 15:16:04 +0000

    cgymd_get function to read the contents of a file from "start" to "stop"

--- cgymd/src/cgymd.h
+++ cgymd/src/cgymd.h
@@ -2,11 +2,22 @@
 #define CGYMD_H_
 #include "libcgym.h"
 
+/*
+ * Structure and function for obtaining the contents of a directory
+ */
 typedef struct file_info FILE_INFO;
 
 struct file_info {
 	cgym_entry_t *entry_file;
 	FILE_INFO *next;
 };
+
+FILE_INFO* list(char* dirname);
+
+/*
+ * Fuction for reading the contents of a file
+ */
+
+char* get(char* start, char* stop, char* filename);
 
 #endif /*CGYMD_H_*/
---
+++ cgymd/src/cgymd_get.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <limits.h>
+#include <sys/types.h>
+#include <errno.h>
+#include "libcgym.h"
+#include "cgymd.h"
+
+char* get(char* start, char* stop, char* filename){
+	unsigned long start_position;
+	unsigned long end_position;
+	char* ep;
+	
+	start_position = strtoul(start, &ep, 10);
+	
+	if (*start == '\0' || *ep != '\0' || *start == '-'){
+		fprintf(stderr,"Not a number\n");
+		return NULL; //got not a number
+	}
+	if (errno == ERANGE && start_position == ULONG_MAX){
+		fprintf(stderr,"Out of range\n");
+		return NULL; // got out of range
+	}
+	
+	end_position = strtoul(stop, &ep, 10);
+	
+	if (*stop == '\0' || *ep != '\0' || *stop == '-'){
+		fprintf(stderr,"Not a number\n");
+		return NULL; //got not a number
+	}
+	if (errno == ERANGE && end_position == ULONG_MAX){
+		fprintf(stderr,"Out of range\n");
+		return NULL; // got out of range
+	}
+	
+	if (start_position >= end_position )
+		return NULL;
+	FILE* f;
+	if ((f= fopen(filename, "r"))==NULL){
+		fprintf(stderr,"Eroare la fopen\n");
+		return NULL;
+	}
+	if (fseek(f,start_position,SEEK_SET)<0){
+		fprintf(stderr,"Eroare la fseek\n");
+		return NULL;		
+	}
+	char* buff = malloc(end_position-start_position+1);
+	if (fread(buff,sizeof(char),end_position-start_position,f)<end_position-start_position){
+		fprintf(stderr,"Eroare la fread");
+		return NULL;
+	}
+	*(buff+(end_position-start_position)) = '\0';
+	return buff;
+}
+

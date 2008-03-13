ff25605e87bc55299c61a877211abda83c88be58
trutia.carmen <trutia.carmen@1d676674-cb47-0410-9e64-db1d7fe6b3aa>
2008-03-13 16:08:41 +0000

    client_handler accepts commands from client and sends error messages in case of wrong commands or wrong arguments

--- trunk/cgymd/src/cgymd.c
+++ trunk/cgymd/src/cgymd.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <ctype.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -11,12 +12,11 @@
 #include <signal.h>
 #include <pthread.h>
 #include "libcgym.h"
-#include "cgymd.h"
 
 #define BACKLOG 10     // how many pending connections queue will hold
 
 void *client_handler(void *p);
-
+char *homedir;
 int main(int argc, char**argv){
 	int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
 	struct sockaddr_in my_addr;    // my address information
@@ -28,6 +28,8 @@ int main(int argc, char**argv){
 		fprintf(stderr,"usage:%s directory [port]",argv[0]);
 		exit(1);
 	}
+	homedir=malloc(strlen(argv[1])*sizeof(char));
+	strncpy(homedir,argv[1],strlen(argv[1]));
 	if (argc==2){
 		printf("Using default port:%d\n",CGYM_DEFAULT_PORT);
 		port = CGYM_DEFAULT_PORT;
@@ -76,11 +78,194 @@ int main(int argc, char**argv){
 	return 0;
 }
 
+char *getNextArg(char *line,int *space){
+	int i,j=0,n=0;
+	char *tmp=NULL;
+	tmp=malloc(255*sizeof(char));
+	for(i=0;i<strlen(line);i++){
+		if((i==0)&&(isspace(line[i]))){
+			n++;
+			continue;
+		}
+		if(!isspace(line[i])){
+			tmp[j]=line[i];
+			j++;
+		}
+		else{
+			if((isspace(line[i-1]))&&(isspace(line[i]))){
+				n++;
+				continue;
+			}
+			*space=n;
+			return tmp;	
+		}
+	}
+	*space=n;
+	return tmp;
+
+}
+int checkCommand(char * cmd){	
+	char * tmp;
+	int i;
+	tmp=malloc(strlen(cmd)*sizeof(char));
+	strncpy(tmp,cmd,strlen(cmd));
+	for(i=0;i<strlen(cmd);i++){
+		tmp[i]=toupper(tmp[i]);
+	}
+	if(strcmp(tmp,"LIST")==0) return 0;
+	if(strcmp(tmp,"SIZE")==0) return 1;
+	if(strcmp(tmp,"GET")==0) return 2;
+	if(strcmp(tmp,"QUIT")==0) return 3;
+	return -1;
+}
 void* client_handler(void *p){
+
 	printf("Handling client request\n");
 	int fd = *(int*)p;
-	if (send(fd, CGYM_ACK_MESSAGE, strlen(CGYM_ACK_MESSAGE), 0) == -1)
+	char bu[20];
+	char *temp,*s2,*comp;
+	char * command,**li,*file=NULL,*dir=NULL,*start=NULL,*stop=NULL;
+	char *buffer,*s=NULL;
+	int r=-1,n=0,size=0,i,t,wrds=0; 
+	if (send(fd, CGYM_ACK_MESSAGE, strlen(CGYM_ACK_MESSAGE), 0) == -1){
 	      perror("send");
+	      exit(1);
+	}
+	do{
+		if((r=recv(fd,bu,sizeof(bu),0))>0){
+			n+=r;
+			if((buffer=realloc(s,n))){
+				s=buffer;
+				strcat(s,bu);
+				size+=r;
+			}
+			else{
+				perror("Error @ realloc!\n");
+				break;
+			}
+		}
+	} while(r==sizeof(bu));
+	if((s[size-3]!='\r')||(s[size-2]!='\n')){
+		if (send(fd, CGYM_ERR_MESSAGE, strlen(CGYM_ERR_MESSAGE), 0) == -1){
+			perror("send");
+		    exit(1);
+		}
+	}
+		if((temp=getNextArg(s,&t))){
+			command=malloc(strlen(temp));
+			strncpy(command,temp,strlen(temp));
+			strncpy(temp,"",strlen(temp));
+		}
+		//check command
+		if((r=checkCommand(command))<0){
+			if (send(fd, CGYM_ERR_MESSAGE, strlen(CGYM_ERR_MESSAGE), 0) == -1){
+				perror("send");
+				exit(1);
+			}
+		}
+		s2=s+strlen(command)+t;
+		s=s2;
+		if((temp=getNextArg(s,&t))){
+			switch(r){
+			case 0:{ //LIST
+				dir=malloc(strlen(temp));
+				strncpy(dir,temp,strlen(temp));
+				strncpy(temp,"",strlen(temp));
+				if(dir==NULL){
+					if (send(fd, CGYM_ERR_MESSAGE, strlen(CGYM_ERR_MESSAGE), 0) == -1){
+						perror("send");
+					    exit(1);
+					}
+				}
+				if(strcmp(homedir,dir)>0){
+					if (send(fd, CGYM_ERR_MESSAGE, strlen(CGYM_ERR_MESSAGE), 0) == -1){
+						perror("send");
+					    exit(1);
+					}
+				}
+				break;
+			}
+			case 1:{ //SIZE
+				file=malloc(strlen(temp));
+				strncpy(file,temp,strlen(temp));
+				strncpy(temp,"",strlen(temp));
+				if(file==NULL){
+					if (send(fd, CGYM_ERR_MESSAGE, strlen(CGYM_ERR_MESSAGE), 0) == -1){
+						perror("send");
+					    exit(1);
+					}
+				}
+				if(file!=strstr(file,homedir)){
+					if (send(fd, CGYM_ERR_MESSAGE, strlen(CGYM_ERR_MESSAGE), 0) == -1){
+						perror("send");
+					    exit(1);
+					}
+				}
+				if(strcmp(homedir,file)>0){
+					if (send(fd, CGYM_ERR_MESSAGE, strlen(CGYM_ERR_MESSAGE), 0) == -1){
+						perror("send");
+					    exit(1);
+					}
+				}
+				break;
+			}
+			case 2:{ //GET
+				start=malloc(strlen(temp));
+				strncpy(start,temp,strlen(temp));
+				strncpy(temp,"",strlen(temp));
+				s2=s+strlen(start)+t;
+				s=s2;
+				if((temp=getNextArg(s,&t))){
+					stop=malloc(strlen(temp));
+					strncpy(stop,temp,strlen(temp));
+					strncpy(temp,"",strlen(temp));
+					s2=s+strlen(stop)+t;
+					s=s2;
+					if((temp=getNextArg(s,&t))){
+						file=malloc(strlen(temp));
+						strncpy(file,temp,strlen(temp));
+						strncpy(temp,"",strlen(temp));
+						s2=s+strlen(stop)+t;
+						s=s2;
+					}
+				}
+				if((file==NULL)||(start==NULL)||(stop==NULL)){
+					if (send(fd, CGYM_ERR_MESSAGE, strlen(CGYM_ERR_MESSAGE), 0) == -1){
+						perror("send");
+					    exit(1);
+					}
+				}
+				if(file!=strstr(file,homedir)){
+					if (send(fd, CGYM_ERR_MESSAGE, strlen(CGYM_ERR_MESSAGE), 0) == -1){
+						perror("send");
+					    exit(1);
+					}
+				}
+				if(strcmp(homedir,file)>0){
+					if (send(fd, CGYM_ERR_MESSAGE, strlen(CGYM_ERR_MESSAGE), 0) == -1){
+						perror("send");
+						exit(1);
+					}
+				}
+				break;
+			}
+			case 3:{ //QUIT
+				if((strlen(temp)>0)&&((temp[0]!='\r')||(temp[1]!='\n'))){
+					if (send(fd, CGYM_ERR_MESSAGE, strlen(CGYM_ERR_MESSAGE), 0) == -1){
+						perror("send");
+						exit(1);
+					}
+				}
+				break;
+			}
+		}
+	}
+		if((temp=getNextArg(s,&t))&&(strcmp(temp,"\r\n")==0)){
+			if (send(fd, CGYM_ERR_MESSAGE, strlen(CGYM_ERR_MESSAGE), 0) == -1){
+				perror("send");
+			    exit(1);
+			}
+		}
 	//exemplu de folosire a functiei list
 	/*
 	FILE_INFO* file_info;
@@ -102,8 +287,16 @@ void* client_handler(void *p){
 		printf("%s\n",file_contents);
 	}
 	*/
+
+	printf("COMANDA:%s\n",command);
+	printf("DIR:%s\n",dir);
+	printf("File:%s\n",file);
+	printf("START:%s\n",start);
+	printf("STOP:%s\n",stop);
+	printf("END!\n");
 	close(fd);
 	pthread_exit(NULL);
+	
 }
 
 

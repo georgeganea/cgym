f696268718dd965c1cf5c1bb4c6a01015ab56c30
ioanaverebi <ioanaverebi@1d676674-cb47-0410-9e64-db1d7fe6b3aa>
2008-03-13 12:05:49 +0000

    Function cgymd_list that returns a list of cgym_entry_t

--- cgymd/src/cgymd.h
+++ cgymd/src/cgymd.h
@@ -1,6 +1,12 @@
 #ifndef CGYMD_H_
 #define CGYMD_H_
+#include "libcgym.h"
 
+typedef struct file_info FILE_INFO;
 
+struct file_info {
+	cgym_entry_t *entry_file;
+	FILE_INFO *next;
+};
 
 #endif /*CGYMD_H_*/

47799756ab51ecdb53e23839fcedc8aa82c42012
mihaitarce <mihaitarce@1d676674-cb47-0410-9e64-db1d7fe6b3aa>
2008-03-14 22:28:18 +0000

    Modified cgym_get().

--- trunk/cgym/src/get.c
+++ trunk/cgym/src/get.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-#include "cgym.h"
+#include <stdlib.h>
+#include "libcgym.h"
 
 int cgym_get(char *remote, char *local, int segments, cgym_server_t **servers) {
 	cgym_sock_t *sock;
 	cgym_server_t **serv;
+	cgym_segment_t **segm;
+	cgym_entry_t *e;
+	
+	int i;
 	
 	// afisam niste informatii
 	printf("getting '%s' in %d segments, saving to '%s'\n",
@@ -28,6 +33,29 @@ int cgym_get(char *remote, char *local, int segments, cgym_server_t **servers) {
  		cgym_server_info_print( cgym_sock_get_server(sock) );
  		printf("\n");
  		return 2;
+ 	}
+ 	
+ 	if (cgym_send_size_req(sock, remote)) {
+ 		cgym_sock_print_info(sock);
+ 		printf("Error: Could not request file information.\n");
+ 		return 3;
+ 	}
+ 	
+ 	if (cgym_recv_size_reply(sock, &e)) {
+ 		cgym_sock_print_info(sock);
+ 		printf("Error: Could not get file information.\n");
+ 		return 4;
+ 	}
+ 	
+ 	printf("entry info: %s [%ld bytes, md5: %s]\n",
+ 			cgym_entry_file(e), cgym_entry_size(e), cgym_entry_md5(e));
+ 	
+ 	if ((segm = malloc(sizeof(*segm) * segments)) == NULL) {
+ 		perror("malloc");
+ 		return 8;
+ 	}
+ 	
+ 	for (i = 0; i < segments; i++) {
  	}
  	
  	printf("GATA!\n");

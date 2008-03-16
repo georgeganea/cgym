471bc30bc3299ce3f3806d727687674808b0720a
mihaitarce <mihaitarce@1d676674-cb47-0410-9e64-db1d7fe6b3aa>
2008-03-16 21:21:07 +0000

    Moved get_file_name function to lib.

--- trunk/cgym/src/cgym.c
+++ trunk/cgym/src/cgym.c
@@ -84,21 +84,3 @@ int main(int argc, char **argv) {
 	
 	return 0;
 }
-
-char *cgym_get_file_name(char *file) {
-	char *ptr, *rc = NULL;
-	
-	if ((ptr = strrchr(file, '/')) == NULL) {
-		ptr = file;
-	} else {
-		ptr++;
-	}
-	
-	rc = malloc(strlen(ptr) + 1);
-		
-	if (rc != NULL) {
-		strcpy(rc, ptr);
-	}
-	
-	return rc;
-}
